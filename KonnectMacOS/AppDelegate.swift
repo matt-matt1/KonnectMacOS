4895862419abe29b9803fea2719cb06d3acf6408
Yuma <yumatechnical@gmail.com>
2020-07-27 18:50:58 -0400

    Initial Commit

---
+++ KonnectMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KonnectMacOS
+//
+//  Created by Yuma Technical Inc. on 2020-07-27.
+//  Copyright © 2020 Yuma Technical Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ KonnectMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KonnectMacOS
+//
+//  Created by Yuma Technical Inc. on 2020-07-27.
+//  Copyright © 2020 Yuma Technical Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
